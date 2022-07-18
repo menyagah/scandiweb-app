1e4e337511eaa13a1798d71f6a45fb599ab8474f
nyagah <nyagah@DESKTOP-31HOFM1.localdomain>
2022-07-18 20:09:25 +0300

    Change app headers

--- public/index.php
+++ public/index.php
@@ -1,36 +1,13 @@
 <?php
 
-// header('Access-Control-Allow-Origin: *');
-// header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Authorization');
-// header("Access-Control-Allow-Credentials: true");
-// header('Content-Type: application/json');
-// $method = $_SERVER['REQUEST_METHOD'];
-// if ($method == "OPTIONS") {
-//     header('Access-Control-Allow-Origin: *');
-//     header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method,Access-Control-Request-Headers, Authorization");
-//     header("HTTP/1.1 200 OK");
-//     die();
-// }
-
-header('Access-Control-Allow-Origin : http://localhost:3000');
-header ("Access-Control-Expose-Headers: Content-Length, X-JSON");
-header ("Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS");
-header ("Access-Control-Allow-Headers: Content-Type, Authorization, Accept, Accept-Language, X-Authorization");
-header('Access-Control-Max-Age: 86400');
-
-if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
-    // The request is using the POST method
-    header("HTTP/1.1 200 OK");
-    return;
-
-}
+header("Cache-Control: no-cache, must-revalidate");
+header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
+header('Access-Control-Allow-Origin:  *');
+header('Access-Control-Allow-Methods:  POST, GET, OPTIONS, PUT, DELETE');
+header('Access-Control-Allow-Headers:  Content-Type, X-Auth-Token, Origin, Authorization');
 
 use app\core\Application;
 use app\controllers\ProductController;
-
-
-
-
 
 require_once __DIR__ . '/../vendor/autoload.php';
 $dotenv = Dotenv\Dotenv::createImmutable(dirname(__DIR__));
@@ -52,4 +29,3 @@ $app->router->get('/products', [new ProductController(), 'product']);
 $app->router->post('/add-product', [new ProductController(), 'createProduct']);
 
 $app->run();
-
